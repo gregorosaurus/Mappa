d3e891517a5c6d01af8e43b9479b5ce61fc6bf22
Greg Mardon <gregmardon@microsoft.com>
2021-09-21 11:22:17 -0600

    Debug tile initial creation.
    Still need to test tile image creation, make sure that still works.

---
+++ src/DebugTileGenerator.cs
@@ -0,0 +1,51 @@
+﻿using SixLabors.Fonts;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Drawing;
+using SixLabors.ImageSharp.Drawing.Processing;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mappa
+{
+    public class DebugTileGenerator : TileGenerator
+    {
+        private Font _font;
+        private Color _color;
+        public DebugTileGenerator(string outputPath) : base(outputPath)
+        {
+            string executionPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string executionDirectory = System.IO.Path.GetDirectoryName(executionPath);
+            string fontPath = System.IO.Path.Combine(executionDirectory, "Roboto-Regular.ttf");
+            FontCollection collection = new FontCollection();
+            collection.Install(fontPath);
+
+            _font = collection.Families.First().CreateFont(16.0f);
+
+            _color = Color.Teal;
+        }
+
+        protected override void CreateIndividualTile(int z, int x, int y)
+        {
+            using (var image = new Image<Rgba32>(256, 256))
+            {
+                string tileText = $"z:{z} x:{x} y:{y}";
+                image.Mutate(x => x.DrawText(tileText, _font, Color.Teal, new PointF(5, 5)));
+
+                IPen pen = Pens.Solid(_color, 3.0f);
+                Rectangle borderPath = new Rectangle(0, 0, 256, 256);
+                image.Mutate(x => x.Draw(pen, borderPath));
+
+                using (FileStream outputStream = new FileStream(GenerateTileSavePath(z, x, y), FileMode.Create, FileAccess.Write))
+                {
+                    image.SaveAsPng(outputStream);
+                }
+            }
+        }
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -9,19 +9,22 @@ namespace Mappa
     {
         class RunParameters
         {
-            [Option('w', "west", Required = true, HelpText = "West longitude in decimal degrees")]
-            public double West { get; set; }
+            [Option('w', "west", Required = false, HelpText = "West longitude in decimal degrees")]
+            public double? West { get; set; }
 
-            [Option('n', "north", Required = true, HelpText = "North latitude in decimal degrees")]
-            public double North { get; set; }
+            [Option('n', "north", Required = false, HelpText = "North latitude in decimal degrees")]
+            public double? North { get; set; }
 
-            [Option('s', "south", Required = true, HelpText = "South latitude in decimal degrees")]
-            public double South { get; set; }
+            [Option('s', "south", Required = false, HelpText = "South latitude in decimal degrees")]
+            public double? South { get; set; }
 
-            [Option('e', "east", Required = true, HelpText = "East longitude in decimal degrees")]
-            public double East { get; set; }
+            [Option('e', "east", Required = false, HelpText = "East longitude in decimal degrees")]
+            public double? East { get; set; }
 
-            [Option('i',"image", Required = true, HelpText = "The path to the image to tile")]
+            [Option('d', "debugtiles", Required = false, Default = false, HelpText = "If set, Mappa will output tiles even if the the image is not available on that tile, with debug information including the tile coordinates")]
+            public bool DebugTiles { get; set; }
+
+            [Option('i',"image", Required = false, HelpText = "The path to the image to tile")]
             public string ImagePath { get; set; }
 
             [Option('z',"zoom", Required = false, Default = 14, HelpText = "The zoom level to tile to (inclusive)")]
@@ -40,21 +43,40 @@ namespace Mappa
                    {
                        Console.WriteLine("Running tiler process");
 
-                       GeoLocation upperRightBounds = GeoLocation.FromDegrees(o.North, o.East);
-                       GeoLocation lowerLeftBounds = GeoLocation.FromDegrees(o.South, o.West);
+                       if (o.DebugTiles)
+                       {
+                           DebugTileGenerator debugTileGenerator = new DebugTileGenerator(o.OutputPath);
+                           debugTileGenerator.MaxLevelOfDetail = o.Zoom;
+                           debugTileGenerator.OutputSaveMode = OutputSaveMode.Folders;
 
-                       Image inputImage = LoadInputImage(o.ImagePath);
+                           debugTileGenerator.CreateTiles();
+                       }
 
-                       ImageTileGenerator imageTileGenerator = new ImageTileGenerator(
-                            upperRightBounds,
-                            lowerLeftBounds,
-                            inputImage,
-                            o.OutputPath);
+                       if (!string.IsNullOrEmpty(o.ImagePath))
+                       {
+                           if (o.East == null || o.West == null || o.North == null || o.South == null)
+                           {
+                               Console.WriteLine("All coordinates must be specified to output an image.");
+                               return;
+                           }
+                           GeoLocation upperRightBounds = GeoLocation.FromDegrees(o.North.Value, o.East.Value);
+                           GeoLocation lowerLeftBounds = GeoLocation.FromDegrees(o.South.Value, o.West.Value);
 
-                       imageTileGenerator.MaxLevelOfDetail = o.Zoom;
-                       imageTileGenerator.OutputSaveMode = OutputSaveMode.Folders;
+                           Image inputImage = LoadInputImage(o.ImagePath);
 
-                       imageTileGenerator.CreateTiles();
+                           ImageTileGenerator imageTileGenerator = new ImageTileGenerator(
+                               upperRightBounds,
+                               lowerLeftBounds,
+                               inputImage,
+                               o.OutputPath);
+
+                           imageTileGenerator.MaxLevelOfDetail = o.Zoom;
+                           imageTileGenerator.OutputSaveMode = OutputSaveMode.Folders;
+
+                           imageTileGenerator.CreateTiles();
+                       }
+
+
                    });
         }
 
